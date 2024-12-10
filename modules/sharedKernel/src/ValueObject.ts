6ca67ac6f19dfce5dabf339c872500915d365da8
Ayoub Adib <adbayb@gmail.com>
2024-12-10 03:17:40 +0100

    feat: implement ValueObject

--- modules/sharedKernel/src/ValueObject.ts
+++ modules/sharedKernel/src/ValueObject.ts
@@ -1,8 +1,7 @@
-import type { AnyRecord } from "./types";
-import type { Result } from "./Result";
-
 /**
- * A value object abstract class to inherit value object behavior.
+ * [Value Objects](https://en.wikipedia.org/wiki/Value_object) are immutable entities whose value is completely determined by their attributes.
+ * They encapsulate data related to a specific concept, such as color, address, phone number, price, etc.
+ * Immutability is the main characteristic of VOs, which means that their values never change once they are initialized.
  *
  * A value object has three main characteristics: no identity, immutability and self validation:
  * - No identity:
@@ -16,25 +15,39 @@ import type { Result } from "./Result";
  * A Value Object must check for the consistency of its values.
  * Every formal validations must happen at value object construction time.
  */
-export const ValueObject = {
-	create<Value extends Result<unknown, unknown>>(
-		getValue: () => Value,
-	): { equals: (input: AnyRecord) => boolean; value: Value } {
-		const value = getValue();
+export abstract class ValueObject<Value> {
+	public readonly value: Value;
 
-		return {
-			equals(equalsInput) {
-				return (
-					JSON.stringify(equalsInput) === JSON.stringify(value.value)
-				);
-			},
-			value,
-		};
-	},
-};
+	public static create(_: unknown) {
+		throw new Error("Must be implemented");
+	}
+
+	protected constructor(input: Value) {
+		this.value = Object.freeze(input); // Immutability is enforced via read-only type and mutation lock (Object.freeze).
+	}
+
+	public equals(input: unknown) {
+		if (this === input) return true;
+
+		return JSON.stringify(this) === JSON.stringify(input);
+	}
+}
 
 /*
- * const Address = ValueObject.create(() => {
- * 	return Result.success({ postalCode: 78370 });
- * });
+ *type AddressValue = {
+ *	country: "France" | "Morocco";
+ *};
+ *
+ *export class Address extends ValueObject<AddressValue> {
+ *	public static override create(input: AddressValue) {
+ *		if (input.country === "Morocco")
+ *			return Result.failure("Country not yet supported");
+ *
+ *		return Result.success(new Address(input));
+ *	}
+ *}
+ *
+ *const result = Address.create({ country: "France" });
+ *
+ *result.type === "success" ? result.payload.value.country : result.payload;
  */
