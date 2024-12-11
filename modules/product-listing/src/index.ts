12063fe738bc14affc21027be955a1146194a11b
Ayoub Adib <adbayb@gmail.com>
2024-12-11 02:30:33 +0100

    feat: review file structure placeholders

--- modules/product-listing/src/index.ts
+++
@@ -1,2 +0,0 @@
-export const HELLO_WORLD = "Hello 👋, please replace me!";
-// TODO: It should expose a factory to init the module in a more conveninent way than instatiating gateways, repositories, and then the use cases on the host side.
