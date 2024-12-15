66e3f9c0db73c71c6d07e6d6707e3aa7c0f637d6
Ayoub Adib <adbayb@gmail.com>
2024-12-15 02:52:42 +0100

    refactor: rename react to web to stick with clean architecture diagram semantics

--- modules/catalog/src/index.ts
+++ modules/catalog/src/index.ts
@@ -2,4 +2,4 @@ export { GetQuotePresenter } from "./adapters/GetQuotePresenter";
 export { GetQuoteController } from "./adapters/GetQuoteController";
 export { QuoteEntityGateway } from "./adapters/QuoteEntityGateway";
 export { GetQuoteUseCase } from "./useCases/GetQuoteUseCase";
-export { CatalogDependencyInjection, useGetQuote } from "./frameworks/react";
+export { CatalogDependencyInjection, useGetQuote } from "./frameworks/web";
